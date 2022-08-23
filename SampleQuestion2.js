cf3889981256e2abeeb6e663c27e331b99760869
Eric Murray <22757609+ermurray@users.noreply.github.com>
2022-08-23 13:14:21 -0600

    Update SampleQuestion2.js

--- SampleQuestion2.js
+++ SampleQuestion2.js
@@ -1,23 +1,3 @@
 // “I am working on a two-player card game. Players can log in and create a game or join an existing game. Every game has a host (the player who created the game) and a guest (the player who joined the game). I was planning to create a games table with a host_player_name, guest_player_name, and score column.“
 
 // “Another mentor told me to pull out some of the columns from the games table into a users table, because right now the data is ‘not normalized’? What does this mean?”
-
-// users
-// PK user_id
-// first_name
-// last_name
-
-// users_games
-// pk id
-// fk user_id 
-// fk game_id
-
-// games
-// Pk game_id
-// fk score
-
-
-//score
-// pk score_id
-// guest score
-// host score
